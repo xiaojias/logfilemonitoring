1eaf5b62f7973f9004125f1f18c76d6799d8385f
xiaojias <649788479@qq.com>
2019-03-15 12:29:05 +0800

    Updated

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ RELEASE="1.0"
 #PLATFORM="linux & win64"
 #PYINSTALLER_IMG_VERSION="1.0"
 num=`echo ${PLATFORM} | grep linux | wc -l`
+cd src
 if [ $num == 1 ]; then
-  cd src
   for i in `ls *.py`
   do
     docker run -v "$(pwd):/src/" richardx/pyinstaller-34-linux:${PYINSTALLER_IMG_VERSION} "pyinstaller --onefile --clean $i"
@@ -14,7 +14,6 @@ fi
 
 num=`echo ${PLATFORM} | grep win64 | wc -l`
 if [ $num == 1 ]; then
-  cd src
   for i in `ls *.py`
   do
     docker run -v "$(pwd):/src/" richardx/pyinstaller-34-win64:${PYINSTALLER_IMG_VERSION} "pyinstaller --onefile --clean $i"
