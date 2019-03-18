54ba7535468c93398872b237ddc19b2ef4193e00
xiaojias <649788479@qq.com>
2019-03-19 00:04:06 +0800

    Pull down docker image

--- build.sh
+++ build.sh
@@ -3,20 +3,24 @@ RELEASE="1.0"
 
 #PLATFORM="linux & win64"
 #PYINSTALLER_IMG_VERSION="1.0"
-num=`echo ${PLATFORM} | grep linux | wc -l`
 cd src
+num=`echo ${PLATFORM} | grep linux | wc -l`
 if [ $num == 1 ]; then
+  DOCKER_IMG="richardx/pyinstaller-34-linux:${PYINSTALLER_IMG_VERSION}"
+  docker pull ${DOCKER_IMG}
   for i in `ls *.py`
   do
-    docker run -v "$(pwd):/src/" richardx/pyinstaller-34-linux:${PYINSTALLER_IMG_VERSION} "pyinstaller --onefile --clean $i"
+    docker run -v "$(pwd):/src/" docker.io/${DOCKER_IMG} "pyinstaller --onefile --clean $i"
   done
 fi
 
 num=`echo ${PLATFORM} | grep win64 | wc -l`
 if [ $num == 1 ]; then
+  DOCKER_IMG="richardx/pyinstaller-34-win64:${PYINSTALLER_IMG_VERSION}"
+  docker pull ${DOCKER_IMG}
   for i in `ls *.py`
   do
-    docker run -v "$(pwd):/src/" richardx/pyinstaller-34-win64:${PYINSTALLER_IMG_VERSION} "pyinstaller --onefile --clean $i"
+    docker run -v "$(pwd):/src/" docker.io/${DOCKER_IMG} "pyinstaller --onefile --clean $i"
   done
 fi
 
